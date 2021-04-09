398a3a7bbfa73bcee25c5314394e152a67a88725
ankita13-1997 <parhiankita@gmail.com>
2021-04-09 22:40:35 +0530

    As a driver i want to unpark my car So that I can go home

--- src/main/java/com/Ankita/Parking_Lot_TC/ParkingLotSystem.java
+++ src/main/java/com/Ankita/Parking_Lot_TC/ParkingLotSystem.java
@@ -1,10 +1,23 @@
 package com.Ankita.Parking_Lot_TC;
 
+
+
 public class ParkingLotSystem {
+    private Object vehicle ;
+
     public ParkingLotSystem() {
     }
 
     public boolean park(Object vehicle) {
+        this.vehicle=vehicle;
         return true;
+    }
+
+    public boolean unPark(Object vehicle) {
+
+        if(this.vehicle.equals(vehicle)){
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/Ankita/Parking_Lot_TC/ParkingLotTest.java
+++ src/test/java/com/Ankita/Parking_Lot_TC/ParkingLotTest.java
@@ -4,17 +4,27 @@ package com.Ankita.Parking_Lot_TC;
 import org.junit.Assert;
 import org.junit.Test;
 
+
 public class ParkingLotTest {
 
     @Test
     public void givenVehicle_WhenParked_ShouldReturnTrue() {
       ParkingLotSystem parkinglotsystem = new ParkingLotSystem();
-        boolean isParked = parkinglotsystem.park(new Object());
-        Assert.assertTrue(isParked);
+      boolean isParked = parkinglotsystem.park(new Object());
+      Assert.assertTrue(isParked);
 
     }
 
     @Test
-    public void givenVehicle_WhenParked_ShouldReturnTrue() {
+    public void givenVehicle_WhenUnParked_ShouldReturnFalse() {
+        Object vehicle = new Object();
+        ParkingLotSystem parkinglotsystem = new ParkingLotSystem();
+        parkinglotsystem.park(vehicle);
+        boolean isUnpark = parkinglotsystem.unPark(vehicle);
+        Assert.assertTrue(isUnpark);
+
     }
+
+
+
 }
